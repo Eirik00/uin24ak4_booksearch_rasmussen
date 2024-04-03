31c94b12a3a689e8585ece58b20477e91098bce2
Eirik00 <eirik.rene@gmail.com>
2024-04-03 17:47:33 +0200

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,59 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [homeContent, setHomeContent] = useState()
+  const [query, setQuery] = useState("james+bond")
+
+  const getData = async()=>{
+    try{
+      const response = await fetch(`https://openlibrary.org/search.json?title=${query}`)
+      const data = await response.json()
+      setHomeContent(data.docs)
+      console.log(data.docs)
+    }catch{
+      console.error("Cant reach API")
+    }
+  }
+
+  useEffect(()=>{
+    getData()
+  },[])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <h1>Eiriks Bibliotek</h1>
+        <nav>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Search</a></li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <section>
+          {homeContent?.map(book => 
+          <article key={book.key}>
+          <h2>{book.title}</h2>
+          <p>Year: {book.first_publish_year}</p>
+          <h3>Authors: </h3>
+          <ul>
+            {book.author_name?.map(author => 
+              <li key={author+book.key}>
+                <p>{author}</p>
+              </li>)}
+          </ul>
+          <p>Average Rating: {!book.ratings_average ? "No Rating" : Math.round(book.ratings_average * 10)/10}</p>
+          <a href={"https://www.amazon.com/s?k="+(book.isbn?book.isbn[0]:"")}>Amazon</a>
+          </article>)}
+        </section>
+      </main>
+      <footer>
+        <p>Eirik Side</p>
+      </footer>
     </>
   )
 }
